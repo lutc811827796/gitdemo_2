9fa494c07265bfb80f2f1237dcfc3987ffed6f96
taichun <811827796@qq.com>
2024-04-08 10:54:31 +0800

    第二次修改

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello,world! -add content");
         //第一次修改，提交并推送到远程GitHub仓库上
+        //第二次修改，提交并推送到远程GitHub仓库上
     }
 }
