db3b225e741029c95d4805606cc2705b38b408a4
taichun <811827796@qq.com>
2024-04-08 13:46:11 +0800

    新建了分支

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello,world! -add content");
         //第一次修改，提交并推送到远程GitHub仓库上
+        
         //第二次修改，提交并推送到远程GitHub仓库上
     }
 }
